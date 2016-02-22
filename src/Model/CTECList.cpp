2207ed5b0fb8b82bb9c9f293518c4236f5321d44
Tyler jarrard <tjar2074@740s-pl-104.local>
2016-02-22 09:30:15 -0700

    CTEC list

---
+++ src/Model/CTECList.cpp
@@ -0,0 +1,88 @@
+/*
+ * CTECList.cpp
+ *
+ *  Created on: Feb 22, 2016
+ *      Author: tjar2074
+ */
+#include "CTECList.h"
+#include <assert.h>
+using namespace std;
+
+template <class Type>
+CTECList<Type>::CTECList()
+{
+
+}
+
+template <class Type>
+CTECList<Type>::~CTECList()
+{
+
+}
+
+template <class Type>
+int CTECList<Type>::getSize()
+{
+
+}
+
+template <class Type>
+void CTECList<Type>::addToFront(Type value)
+{
+
+}
+
+template <class Type>
+void CTECList<Type>::addToEnd(Type value)
+{
+
+}
+
+template <class Type>
+void CTECList<Type>::AddAtIndex(int index, Type value)
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::getFront()
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::getEnd()
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::getFromIndex(int index)
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::removeFromFront()
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::removeFromEnd()
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::removeFromIndex(int index)
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>::Set(int index, Type value)
+{
+
+}
+
